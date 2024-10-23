52eb34ba714cb30093ed7f833326c5c61158ff8e
WooLegend <dnwogus4260@naver.com>
2024-10-23 20:32:23 +0900

    chore: initialize project with basic setup
    - Set up React application using `npx create-react-app`
    - Install prettier and tailwind CSS
    - Create initial dir structure
    - Downgradeed TypeScript to V 4.9.5 to resoleve onfilicts with
      prettier-plugin-tailwindcss

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# openmind
+# Open Mind
+
+## Setup
+
+### 1. 모듈 다운로드
+
+```
+npm i
+```
+
+### 2. 에디터 포메터 설정
+
+1. Prettier 확장 프로그램 설치
+2. `setting -> Default formatter -> prettier`
+3. `setting -> Format On Save -> 체크`
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./globar.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
