60378f883c2eb326546adb7fa6e900733a84e864
haksoo <haksoo0918@gmail.com>
2024-10-24 20:29:55 +0900

    설정, 추가: eslint 설정 및 관련 패키지 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,26 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'prettier', // npm install --save-dev eslint-config-prettier
+    ],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+      'react/prop-types': 'off',
+    },
+  },
 ];
