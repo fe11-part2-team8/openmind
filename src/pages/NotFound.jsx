0733293cd3094664c8d48b55778920ca18991cf7
JUYOUNG\vilio <vilioite@naver.com>
2024-10-30 15:47:07 +0900

    feat: notfound 페이지 완성

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 import '../global.css';
+import warning from '../assets/images/warning.png';
 
 function NotFound() {
   return (
     <div className="mt-[120px] flex flex-col items-center">
-      <h1 className="h1 font-bold">페이지가 없거나 접근할 수 없어요.</h1>
-      <p className="body1 mt-3">입력하신 주소가 맞는지 다시 확인해 주세요.</p>
+      <img src={warning} alt="warning" />
+      <h1 className="h1 mt-7 font-bold">페이지가 없거나 접근할 수 없어요.</h1>
+      <p className="body1 mt-4">입력하신 주소가 맞는지 다시 확인해 주세요.</p>
       <Link to="/">
-        <button>오픈마인드 홈으로</button>
+        <button className="btn btn-rounded mt-[40px]">오픈마인드 홈으로</button>
       </Link>
     </div>
   );
