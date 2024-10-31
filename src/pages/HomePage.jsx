51138f9b022857d22595fa36b0a17f3f74678f9d
서현우 <s0sew.com@gmail.com>
2024-11-01 01:10:35 +0900

    feat: css 스타일 조정 중

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import styles from './HomePage.module.css';
-
-import logo from '../assets/images/logo.svg';
-import homepage from '../assets/images/main-background.png';
+// import styles from './HomePage.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as IconArrowRight } from '../assets/images/icon-arrow-right.svg';
+import logo from '../assets/images/logo.svg'; // 이미지 가져오기
+import homeImg from '../assets/images/main-background.png'; // 이미지 가져오기
 import SubjectCreateForm from '../components/SubjectCreateForm';
 
 function HomePage() {
   return (
-    <div className={styles.homepage}>
-      <img src={logo} alt="Open Mind" width="456" height="180" />
-      <SubjectCreateForm />
-      <img src={homepage} alt="HomePage bottom img" width="1200" height="627" />
+    <div className="grid-rows=[auto_auto_auto] grid min-h-screen gap-8 bg-white p-6">
+      <div className="mt-20 flex flex-col items-center">
+        <img src={logo} alt="Open Mind" className="mb-6 w-60" />
+        <Link to="/list" className="btn btn-outline">
+          질문하러 가기
+          <IconArrowRight width="18" height="18" />
+        </Link>
+      </div>
+
+      <div className="grid grid-rows-[auto_auto] gap-4">
+        <SubjectCreateForm />
+      </div>
+
+      <div className="w-full">
+        <img src={homeImg} alt="HomePage bottom img" className="h-auto w-full object-contain" />
+      </div>
     </div>
   );
 }
