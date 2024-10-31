08d050a095917453c2aee4b436276483be833001
서현우 <s0sew.com@gmail.com>
2024-10-31 18:33:57 +0900

    feat: 메인페이지 jsx작성

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-// import HomePage from './pages/HomePage';
-import SubjectCreateForm from './components/SubjectCreateForm';
+import HomePage from './pages/HomePage';
 import SubjectListPage from './pages/SubjectList.page';
 import PostPage from './pages/PostPage';
 import AnswerPage from './pages/AnswerPage';
 import QuestionCreateModal from './components/QuestionCreateModal';
-import AnswerCreateForm from './components/AnswerCreateForm';
 import AdminPage from './pages/Admin/Admin.page';
 import NotFound from './pages/NotFound';
 
@@ -15,10 +13,8 @@ function Main() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          {/* <Route index element={<HomePage />} /> */}
-          <Route index element={<SubjectCreateForm />} />
+          <Route index element={<HomePage />} />
           <Route path="list" element={<SubjectListPage />} />
-          <Route path="answercreateform" element={<AnswerCreateForm />} />
           <Route path="modal" element={<QuestionCreateModal />} />
           <Route path="admin" element={<AdminPage />} />
           <Route path="post">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import styles from './HomePage.module.css';
+
+import logo from '../assets/images/logo.svg';
+import homepage from '../assets/images/main-background.png';
+import SubjectCreateForm from '../components/SubjectCreateForm';
+
 function HomePage() {
-  return <h1 className="h1">Home Page</h1>;
+  return (
+    <div className={styles.homepage}>
+      <img src={logo} alt="Open Mind" width="456" height="180" />
+      <SubjectCreateForm />
+      <img src={homepage} alt="HomePage bottom img" width="1200" height="627" />
+    </div>
+  );
 }
 
 export default HomePage;
