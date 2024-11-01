2bd9caf121ec680e106f50711df1cfd54ecb0110
WooLegend <dnwogus4260@naver.com>
2024-11-01 14:22:07 +0900

    충동 해결

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-=======
 import React from 'react';
 import styles from './HomePage.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as IconArrowRight } from '../assets/images/icon-arrow-right.svg';
 import logo from '../assets/images/logo.svg'; // 이미지 가져오기
->>>>>>> origin/develop
 import SubjectCreateForm from '../components/SubjectCreateForm';
 
 function HomePage() {
   return (
-<<<<<<< HEAD
-    <div>
-      <SubjectCreateForm />
-      <Link to={'/list'}>질문 작성하러 가기</Link>
-=======
     <div className={styles.wrap}>
       <div className="container mx-auto flex flex-col items-center">
         <Link to="/list" className="btn btn-outline self-end">
@@ -27,7 +18,6 @@ function HomePage() {
 
         <SubjectCreateForm />
       </div>
->>>>>>> origin/develop
     </div>
   );
 }
