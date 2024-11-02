1d74c8c6cb6cd08c2c82d9dfaee6885bef15e121
WooLegend <dnwogus4260@naver.com>
2024-11-03 04:25:48 +0900

    feat: 답변 거절 비동기 처리

--- src/components/QuestionListItem/QuestionAndAnswer.jsx
+++ src/components/QuestionListItem/QuestionAndAnswer.jsx
@@ -5,12 +5,24 @@ import Dropdown from './Dropdown/index';
 import AnswerCreateAndEdit from '../AnswerCreateAndEdit/index';
 import ReactionButtons from './ReactionButton';
 import styles from './QuestionListItem.module.css';
+import useAsync from '../../hooks/useAsync';
 
 const IS_REJECTED = true;
+const REJECTED_CONTENT = '거절된 답변입니다.';
 
 function QuestionAndAnswer({ question, name, isSubjectOwner, imageSource, onUpdate }) {
   const { like, dislike, answer } = question;
   const [isEditMode, setIsEditMode] = useState(false); // 수정 모드 상태
+  const {
+    loading: loadingPost,
+    error: errorPost,
+    wrappedFunction: postAnswerAsync,
+  } = useAsync(postAnswer);
+  const {
+    loading: loadingPatch,
+    error: errorPatch,
+    wrappedFunction: patchAnswerAsync,
+  } = useAsync(patchAnswer);
 
   console.log(isSubjectOwner);
 
@@ -27,18 +39,15 @@ function QuestionAndAnswer({ question, name, isSubjectOwner, imageSource, onUpda
 
   // 답변 거절 핸들러
   const handleRefuse = async () => {
-    try {
-      if (answer) {
-        // 답변이 이미 있을 때는 거절 상태로 업데이트
-        await patchAnswer('', answer.id, IS_REJECTED);
-      } else {
-        // 답변이 없을 때는 거절된 기본 답변 생성
-        await postAnswer('', question.id, IS_REJECTED);
-      }
-      onUpdate();
-    } catch (error) {
-      alert('답변 거절에 실패했습니다.');
+    if (answer) {
+      console.log(answer);
+      await patchAnswerAsync(REJECTED_CONTENT, answer.id, IS_REJECTED);
+      if (errorPatch) alert(errorPatch);
+    } else {
+      await postAnswerAsync(REJECTED_CONTENT, question.id, IS_REJECTED);
+      if (errorPost) alert(errorPost);
     }
+    onUpdate();
   };
 
   // 새 답변 추가 핸들러
@@ -75,14 +84,7 @@ function QuestionAndAnswer({ question, name, isSubjectOwner, imageSource, onUpda
           setIsEditMode={setIsEditMode}
         />
       ) : (
-        <AnswerContentItem
-          name={name}
-          imageSource={imageSource}
-          answerDate={answer?.createdAt}
-          isRejected={answer?.isRejected}
-          currentAnswer={answer}
-          questionId={question.id}
-        />
+        <AnswerContentItem answer={answer} name={name} imageSource={imageSource} />
       )}
 
       <ReactionButtons questionId={question.id} initialLikes={like} initialDislikes={dislike} />
@@ -90,39 +92,31 @@ function QuestionAndAnswer({ question, name, isSubjectOwner, imageSource, onUpda
   );
 }
 
-function AnswerContentItem({
-  name,
-  imageSource,
-  answerDate = null,
-  isRejected = false,
-  currentAnswer,
-}) {
-  return (
-    <>
-      {currentAnswer && (
-        <div className={`${styles.answerContainer} text-left`}>
-          <div className={styles.profile}>
-            <img
-              src={imageSource}
-              alt={`${name}의 프로필 이미지`}
-              style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }}
-            />
-            <div className={styles.answer}>
-              <div className={styles.nameday}>
-                <p>{name}</p>
-                <p className={styles.day}>{date(answerDate)}</p>
-              </div>
+function AnswerContentItem({ answer, name, imageSource }) {
+  if (!answer) return <></>;
 
-              {isRejected ? (
-                <p className="text-base text-[#B93333]">답변 거절</p>
-              ) : (
-                <p className={styles.answerValue}>{currentAnswer.content}</p>
-              )}
-            </div>
+  return (
+    <div className={`${styles.answerContainer} text-left`}>
+      <div className={styles.profile}>
+        <img
+          src={imageSource}
+          alt={`${name}의 프로필 이미지`}
+          style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }}
+        />
+        <div className={styles.answer}>
+          <div className={styles.nameday}>
+            <p>{name}</p>
+            <p className={styles.day}>{date(answer.createAt)}</p>
           </div>
+
+          {answer.isRejected ? (
+            <p className="text-base text-[#B93333]">답변 거절</p>
+          ) : (
+            <p className={styles.answerValue}>{answer.content}</p>
+          )}
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
 
