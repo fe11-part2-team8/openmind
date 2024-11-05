3611a5798a0a68768d97cd63e0804eaa63e7806d
WooLegend <dnwogus4260@naver.com>
2024-11-05 16:19:47 +0900

    feat: App.js에 디바이스 타입 Provider 적용

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import PostPage from './pages/PostPage';
 import AdminPage from './pages/Admin/Admin.page';
 import NotFound from './pages/NotFound';
 import './global.css';
+import { DeviceTypeProvider } from './contexts/DeviceTypeContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* TODO param을 옵션으로 받는 더 좋은 방법? */}
-        <Route path="list" element={<SubjectListPage />}>
-          <Route path=":pageNum" element={<SubjectListPage />} />
-        </Route>
-        <Route path="admin" element={<AdminPage />} />
-        <Route path="post">
-          <Route path=":id">
-            <Route index element={<PostPage />} />
+    <DeviceTypeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* TODO param을 옵션으로 받는 더 좋은 방법? */}
+          <Route path="list" element={<SubjectListPage />}>
+            <Route path=":pageNum" element={<SubjectListPage />} />
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="admin" element={<AdminPage />} />
+          <Route path="post">
+            <Route path=":id">
+              <Route index element={<PostPage />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </DeviceTypeProvider>
   );
 }
 
