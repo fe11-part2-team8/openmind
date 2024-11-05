ef2f59b1cbb789bab45da7b13b9b7fb9cef1f7f7
서현우 <s0sew.com@gmail.com>
2024-11-05 20:32:03 +0900

    feat: trim() 추가

--- src/components/SubjectCreateForm/index.jsx
+++ src/components/SubjectCreateForm/index.jsx
@@ -6,9 +6,11 @@ import icon_person from '../../assets/images/icon-person.svg';
 import styles from './SubjectCreateForm.module.css';
 import Loading from '../Loading';
 
+const NAME_LENGTH_ERROR = '이름은 30자 이내로 적어주세요.';
+
 function checkNameValid(name) {
-  if (name.length < 1 || name.length > 30) return false;
-  return true;
+  const _name = name.trim();
+  return _name.length > 0 && _name.length <= 30;
 }
 
 function SubjectCreateForm() {
@@ -19,6 +21,10 @@ function SubjectCreateForm() {
   // 서브젝트 생성, 페이지 이동
   const handleCreateSubject = async (event) => {
     event.preventDefault(); // 폼 제출 시 페이지 새로고침 방지
+
+    if (!checkNameValid(name)) {
+      return; // 유효하지 않은 경우 폼 제출 방지
+    }
 
     const result = await postSubjectAsync(name);
     if (error) {
@@ -31,13 +37,16 @@ function SubjectCreateForm() {
   };
 
   const handleChangeName = (e) => {
-    setName(e.target.value.trim());
+    setName(e.target.value);
   };
 
   return (
     <form className={styles.subjectCreateForm} onSubmit={handleCreateSubject}>
       <Loading isVisible={loading} />
-      <InputName name={name} onChange={handleChangeName} />
+      <div className={styles.inputContainer}>
+        {name.length > 30 && <p className={styles.errorMessage}>{NAME_LENGTH_ERROR}</p>}
+        <InputName name={name} onChange={handleChangeName} />
+      </div>
       <button type="submit" disabled={!checkNameValid(name)}>
         질문 받기
       </button>
